dda3c309ed16e6883259c140622c6dca0de4b4a4
EverOddish <everoddish@gmail.com>
2019-09-08 10:31:39 -0400

    Added move data and validation

--- validate.py
+++ validate.py
@@ -6,19 +6,26 @@ import sys
 if not shutil.which('xmllint'):
     raise SystemExit('xmllint not found!')
 
-invalid_pokemon = []
+invalid_files = []
 
-for filename in os.listdir(os.path.join('xml', 'pokemon')):
-    path = os.path.join('xml', 'pokemon', filename)
-    command = ['xmllint', '--noout', '--schema', 'xsd/pokemon.xsd', path]
+def run_xmllint(schema, target):
+    command = ['xmllint', '--noout', '--schema', schema, target]
     result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if 'validates' not in result.stderr.decode('utf-8'):
-        invalid_pokemon.append(path)
+        invalid_files.append(target)
+        print('Failed to validate: ' + target)
 
-for invalid in invalid_pokemon:
-    print('Failed to validate: ' + invalid)
+schema = os.path.join('xsd', 'pokemon.xsd')
+for filename in os.listdir(os.path.join('xml', 'pokemon')):
+    target = os.path.join('xml', 'pokemon', filename)
+    run_xmllint(schema, target)
 
-if len(invalid_pokemon) > 0:
+schema = os.path.join('xsd', 'move.xsd')
+for filename in os.listdir(os.path.join('xml', 'moves')):
+    target = os.path.join('xml', 'moves', filename)
+    run_xmllint(schema, target)
+
+if len(invalid_files) > 0:
     raise SystemExit("Invalid data!")
 else:
     print("All data is valid!")
