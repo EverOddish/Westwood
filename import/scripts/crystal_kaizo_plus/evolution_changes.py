938a2242c21a08b8c0d5781ef9ad3e7ac1ac1558
EverOddish <everoddish@gmail.com>
2022-05-22 10:10:53 -0400

    Added some Crystal Kaizo+ scripts

---
+++ import/scripts/crystal_kaizo_plus/evolution_changes.py
@@ -0,0 +1,43 @@
+import os
+import re
+import sys
+
+sys.path.append(os.getcwd())
+from intermediate.pokemon import Pokemon, StatSet, TypeSet
+from intermediate.pokemon import Pokemon, AbilitySet, AbilityRecord
+
+evolutions = []
+with open('scripts/crystal_kaizo_plus/evolutions.csv') as f:
+    pokemon = None
+    changes = None
+    stats = False
+    lines = f.readlines()
+    for line in lines:
+        line = line.split('\n')[0]
+        chunks = line.split(',')
+        pokemon = chunks[0]
+        level = chunks[2]
+        evolves_to = chunks[4]
+        evolutions.append((pokemon, level, evolves_to))
+
+#print(evolutions)
+
+for pokemon, level, evolves_to in evolutions:
+    filename = pokemon.lower().replace(' ', '_').replace('\'', '_').replace('-', '_') + '.xml'
+    path = os.path.join('..', 'xml', 'pokemon', filename)
+    pokemon_object = Pokemon(path)
+
+    evolution_copy = pokemon_object.copy_evolution_set("Pokemon Crystal")
+    evolution_copy.games = ["Pokemon Crystal Kaizo+"]
+    found = False
+    for record in evolution_copy.evolution_records:
+        if record.evolves_to == evolves_to:
+            found = True
+            if len(level) > 0:
+                record.level = level
+                #print(f"Updated {pokemon}")
+            else:
+                #print("Item")
+                pass
+    if not found:
+        print(f"Not handled: {pokemon}")
---
+++ import/scripts/crystal_kaizo_plus/learnset_changes.py
@@ -0,0 +1,52 @@
+import os
+import re
+import sys
+
+sys.path.append(os.getcwd())
+from intermediate.learnset import PokemonLearnset, Learnset
+
+all_pokemon = {}
+
+with open('scripts/crystal_kaizo_plus/learnsets.csv') as f:
+    pokemon = None
+    changes = []
+    lines = f.readlines()
+    for line in lines:
+        line = line.split('\n')[0]
+        result = re.match(r'([A-Za-z\' _\-]+),', line)
+        if result:
+            if pokemon:
+                all_pokemon[pokemon] = changes
+            pokemon = result.group(1)
+            if "Farfetch D" == pokemon:
+                pokemon = "FarfetchD"
+            if "Mr  Mime" == pokemon:
+                pokemon = "Mr Mime"
+            changes = []
+        else:
+            result = re.match(r'(\d)+,([A-Za-z\' _\-]+)', line)
+            if result:
+                changes.append(line)
+    all_pokemon[pokemon] = changes
+
+for pokemon in all_pokemon.keys():
+    filename = pokemon.lower().replace(' ', '_').replace('\'', '_').replace('-', '_') + '.xml'
+    path = os.path.join('..', 'xml', 'learnsets', filename)
+    learnset = PokemonLearnset(path)
+    changes = all_pokemon[pokemon]
+
+    learnset_copy = Learnset()
+    learnset_copy.games = ['Pokemon Crystal Kaizo+']
+    for line in changes:
+        result = re.match(r'(\d+),([A-Za-z \-\']+)', line)
+        if result:
+            level = result.group(1)
+            level = int(level)
+            level = str(level)
+            move = result.group(2)
+            learnset_copy.moves.append((level, move))
+
+    learnset.add_learnset(learnset_copy)
+    print("Added " + pokemon)
+    #with open(path, 'w') as g:
+    #    learnset.dump(g)
